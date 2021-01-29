0920517b150e4795e670dfbae7b3a20e9e1b0daa
Alexander Fedulov <1492164+afedulov@users.noreply.github.com>
2021-01-29 15:18:18 +0100

    [FLINK-13550][ui] Add job vertex FlameGraph UI

---
+++ flink-runtime-web/web-dashboard/src/app/share/customize/flame-graph/flame-graph.component.ts
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, ChangeDetectionStrategy, ElementRef, Input, ViewChild } from '@angular/core';
+import { JobFlameGraphNodeInterface } from 'interfaces';
+import { select } from 'd3-selection';
+import { flamegraph } from 'd3-flame-graph';
+
+@Component({
+  selector: 'flink-flame-graph',
+  templateUrl: './flame-graph.component.html',
+  styleUrls: [],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FlameGraphComponent {
+  @ViewChild('flameGraphContainer') flameGraphContainer: ElementRef<Element>;
+  @Input() data: JobFlameGraphNodeInterface;
+
+  draw() {
+    if (this.data) {
+      const element = this.flameGraphContainer.nativeElement;
+      select(element)
+        .selectAll('*')
+        .remove();
+      select(element)
+        .datum(this.data)
+        .call(flamegraph().width(element.clientWidth));
+    }
+  }
+}
