b560131873cb38254daffddb4e4487785627233b
Alexander Fedulov <1492164+afedulov@users.noreply.github.com>
2021-11-24 00:35:24 +0100

    [hotfix] rename tests

--- flink-formats/flink-csv/src/test/java/org/apache/flink/formats/csv/CsvFormatITCase.java
+++ flink-formats/flink-csv/src/test/java/org/apache/flink/formats/csv/CsvFormatITCase.java
@@ -18,360 +18,158 @@
 
 package org.apache.flink.formats.csv;
 
-import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.restartstrategy.RestartStrategies;
-import org.apache.flink.connector.file.src.FileSource;
-import org.apache.flink.connector.file.src.reader.DelimitedFormat;
-import org.apache.flink.connector.file.src.reader.StreamFormat;
-import org.apache.flink.core.fs.Path;
-import org.apache.flink.runtime.testutils.MiniClusterResourceConfiguration;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.DataStreamUtils;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.operators.collect.ClientAndIterator;
-import org.apache.flink.table.data.RowData;
-import org.apache.flink.table.data.util.DataFormatConverters;
-import org.apache.flink.table.runtime.typeutils.InternalTypeInfo;
-import org.apache.flink.table.types.DataType;
-import org.apache.flink.table.types.logical.RowType;
-import org.apache.flink.test.util.MiniClusterWithClientResource;
-import org.apache.flink.types.Row;
-import org.apache.flink.util.TestLogger;
-import org.apache.flink.util.function.FunctionWithException;
+import org.apache.flink.formats.common.TimeFormats;
+import org.apache.flink.table.planner.runtime.utils.TestData;
+import org.apache.flink.table.planner.utils.JavaScalaConversionUtil;
 
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
-import org.junit.ClassRule;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.math.BigDecimal;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
-import static org.apache.flink.table.api.DataTypes.BIGINT;
-import static org.apache.flink.table.api.DataTypes.DECIMAL;
-import static org.apache.flink.table.api.DataTypes.FIELD;
-import static org.apache.flink.table.api.DataTypes.ROW;
-import static org.apache.flink.table.api.DataTypes.STRING;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.apache.flink.table.utils.DateTimeUtils.unixTimestampToLocalDateTime;
 
-/** MiniCluster-based integration test for reading CSV data from {@link FileSource}. */
-public class CsvFormatITCase extends TestLogger {
+// based on TableSourceJsonPlanITCase
+public class CsvFormatITCase extends CsvJsonPlanTestBase {
 
-    private static final int PARALLELISM = 4;
-
-    @ClassRule public static final TemporaryFolder TMP_FOLDER = new TemporaryFolder();
-
-    //    @ClassRule
-    public static final MiniClusterWithClientResource miniClusterResource =
-            new MiniClusterWithClientResource(
-                    new MiniClusterResourceConfiguration.Builder()
-                            .setNumberTaskManagers(1)
-                            .setNumberSlotsPerTaskManager(PARALLELISM)
-                            .build());
-
-    // ------------------------------------------------------------------------
-    //  test cases
-    // ------------------------------------------------------------------------
-
-    /** This test runs a job reading bounded input with a stream record format (text lines). */
     @Test
-    public void testBoundedTextFileSourceWithDeserializationSchema() throws Exception {
-        final File testDir = TMP_FOLDER.newFolder();
-        writeFile(testDir, "data.csv", CSV_LINES);
+    public void testProjectPushDown() throws Exception {
+        List<String> data = Arrays.asList("1,1,hi", "2,1,hello", "3,2,hello world");
+        createTestCsvSourceTable("MyTable", data, "a bigint", "b int not null", "c varchar");
+        File sinkPath = createTestCsvSinkTable("MySink", "a bigint", "c varchar");
 
-        final Row[] expected =
-                new Row[] {
-                    Row.of(
-                            "Berlin",
-                            new BigDecimal("52.5167"),
-                            new BigDecimal("13.3833"),
-                            "Germany",
-                            "DE",
-                            "Berlin",
-                            "primary",
-                            3644826L),
-                    Row.of(
-                            "San Francisco",
-                            new BigDecimal("37.7562"),
-                            new BigDecimal("-122.4430"),
-                            "United States",
-                            "US",
-                            "California",
-                            "",
-                            3592294L),
-                    Row.of(
-                            "Beijing",
-                            new BigDecimal("39.9050"),
-                            new BigDecimal("116.3914"),
-                            "China",
-                            "CN",
-                            "Beijing",
-                            "primary",
-                            19433000L)
-                };
+        String jsonPlan = tableEnv.getJsonPlan("insert into MySink select a, c from MyTable");
+        tableEnv.executeJsonPlan(jsonPlan).await();
 
-        DataType dataType =
-                ROW(
-                        FIELD("city", STRING()),
-                        FIELD("lat", DECIMAL(8, 4)),
-                        FIELD("lng", DECIMAL(8, 4)),
-                        FIELD("country", STRING()),
-                        FIELD("iso2", STRING()),
-                        FIELD("admin_name", STRING()),
-                        FIELD("capital", STRING()),
-                        FIELD("population", BIGINT()));
-        RowType rowType = (RowType) dataType.getLogicalType();
-
-        CsvRowDataDeserializationSchema deserSchema =
-                new CsvRowDataDeserializationSchema.Builder(rowType, InternalTypeInfo.of(rowType))
-                        .build();
-
-        DelimitedFormat<RowData> csvFormat = DelimitedFormat.of("\n", deserSchema);
-
-        final List<RowData> result = initializeSourceAndReadData(testDir, csvFormat);
-
-        verifyResult(dataType, expected, result);
+        assertResult(Arrays.asList("1,hi", "2,hello", "3,hello world"), sinkPath);
     }
 
     @Test
-    public void testBoundedTextFileSourceWithJacksonAsPojo() throws Exception {
-        final File testDir = TMP_FOLDER.newFolder();
-        writeFile(testDir, "data.csv", CSV_LINES);
+    public void testReadingMetadata() throws Exception {
+        createTestValuesSourceTable(
+                "MyTable",
+                JavaScalaConversionUtil.toJava(TestData.smallData3()),
+                new String[] {"a int", "b bigint", "m varchar metadata"},
+                new HashMap<String, String>() {
+                    {
+                        put("readable-metadata", "m:STRING");
+                    }
+                });
 
-        final CsvFormat<CityPojo> csvFormat = CsvFormat.from(CityPojo.class);
-        final List<CityPojo> result = initializeSourceAndReadData(testDir, csvFormat);
+        File sinkPath = createTestCsvSinkTable("MySink", "a bigint", "m varchar");
 
-        final CityPojo[] expected =
-                new CityPojo[] {
-                    new CityPojo(
-                            "Berlin",
-                            new BigDecimal("52.5167"),
-                            new BigDecimal("13.3833"),
-                            "Germany",
-                            "DE",
-                            "Berlin",
-                            "primary",
-                            3644826L),
-                    new CityPojo(
-                            "San Francisco",
-                            new BigDecimal("37.7562"),
-                            new BigDecimal("-122.4430"),
-                            "United States",
-                            "US",
-                            "California",
-                            "",
-                            3592294L),
-                    new CityPojo(
-                            "Beijing",
-                            new BigDecimal("39.9050"),
-                            new BigDecimal("116.3914"),
-                            "China",
-                            "CN",
-                            "Beijing",
-                            "primary",
-                            19433000L)
-                };
+        String jsonPlan = tableEnv.getJsonPlan("insert into MySink select a, m from MyTable");
+        tableEnv.executeJsonPlan(jsonPlan).await();
 
-        assertEquals(Arrays.asList(expected), result);
+        assertResult(Arrays.asList("1,Hi", "2,Hello", "3,Hello world"), sinkPath);
     }
 
-    @JsonPropertyOrder({
-        "city",
-        "lat",
-        "lng",
-        "country",
-        "iso2",
-        "adminName",
-        "capital",
-        "population"
-    })
-    public static class CityPojo {
-        public String city;
-        public BigDecimal lat;
-        public BigDecimal lng;
-        public String country;
-        public String iso2;
-        public String adminName;
-        public String capital;
-        public long population;
+    @Test
+    public void testFilterPushDown() throws Exception {
+        List<String> data = Arrays.asList("1,1,hi", "2,1,hello", "3,2,hello world");
+        createTestCsvSourceTable("MyTable", data, "a bigint", "b int not null", "c varchar");
+        File sinkPath = createTestCsvSinkTable("MySink", "a bigint", "b int", "c varchar");
 
-        public CityPojo() {};
+        String jsonPlan =
+                tableEnv.getJsonPlan("insert into MySink select * from MyTable where a > 1");
+        tableEnv.executeJsonPlan(jsonPlan).await();
 
-        public CityPojo(
-                String city,
-                BigDecimal lat,
-                BigDecimal lng,
-                String country,
-                String iso2,
-                String admin_name,
-                String capital,
-                long population) {
-            this.city = city;
-            this.lat = lat;
-            this.lng = lng;
-            this.country = country;
-            this.iso2 = iso2;
-            this.adminName = admin_name;
-            this.capital = capital;
-            this.population = population;
-        }
-
-        @Override
-        public String toString() {
-            return "CitiesPojo{"
-                    + "city='"
-                    + city
-                    + '\''
-                    + ", lat="
-                    + lat
-                    + ", lng="
-                    + lng
-                    + ", country='"
-                    + country
-                    + '\''
-                    + ", iso2='"
-                    + iso2
-                    + '\''
-                    + ", adminName='"
-                    + adminName
-                    + '\''
-                    + ", capital='"
-                    + capital
-                    + '\''
-                    + ", population="
-                    + population
-                    + '}';
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            CityPojo that = (CityPojo) o;
-            return population == that.population
-                    && Objects.equals(city, that.city)
-                    && Objects.equals(lat, that.lat)
-                    && Objects.equals(lng, that.lng)
-                    && Objects.equals(country, that.country)
-                    && Objects.equals(iso2, that.iso2)
-                    && Objects.equals(adminName, that.adminName)
-                    && Objects.equals(capital, that.capital);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(city, lat, lng, country, iso2, adminName, capital, population);
-        }
+        assertResult(Arrays.asList("2,1,hello", "3,2,hello world"), sinkPath);
     }
 
-    private static <T> List<T> initializeSourceAndReadData(File testDir, StreamFormat<T> csvFormat)
-            throws Exception {
-        final FileSource<T> source =
-                FileSource.forRecordStreamFormat(csvFormat, Path.fromLocalFile(testDir)).build();
+    @Test
+    public void testPartitionPushDown() throws Exception {
+        createTestValuesSourceTable(
+                "MyTable",
+                JavaScalaConversionUtil.toJava(TestData.smallData3()),
+                new String[] {"a int", "p bigint", "c varchar"},
+                "p",
+                new HashMap<String, String>() {
+                    {
+                        put("partition-list", "p:1;p:2");
+                    }
+                });
+        File sinkPath = createTestCsvSinkTable("MySink", "a int", "p bigint", "c varchar");
 
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(PARALLELISM);
-        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(1, 0));
+        String jsonPlan =
+                tableEnv.getJsonPlan("insert into MySink select * from MyTable where p = 2");
+        tableEnv.executeJsonPlan(jsonPlan).await();
 
-        final DataStream<T> stream =
-                env.fromSource(source, WatermarkStrategy.noWatermarks(), "file-source");
-
-        final ClientAndIterator<T> client =
-                DataStreamUtils.collectWithClient(stream, "Bounded TextFiles Test");
-
-        final List<T> result = new ArrayList<>();
-        while (client.iterator.hasNext()) {
-            T next = client.iterator.next();
-            result.add(next);
-        }
-        return result;
+        assertResult(Arrays.asList("2,2,Hello", "3,2,Hello world"), sinkPath);
     }
 
-    // ------------------------------------------------------------------------
-    //  verification
-    // ------------------------------------------------------------------------
+    @Test
+    public void testWatermarkPushDown() throws Exception {
+        createTestValuesSourceTable(
+                "MyTable",
+                JavaScalaConversionUtil.toJava(TestData.data3WithTimestamp()),
+                new String[] {
+                    "a int",
+                    "b bigint",
+                    "c varchar",
+                    "ts timestamp(3)",
+                    "watermark for ts as ts - interval '5' second"
+                },
+                new HashMap<String, String>() {
+                    {
+                        put("enable-watermark-push-down", "true");
+                    }
+                });
 
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    private static void verifyResult(DataType dataType, Row[] expected, List<RowData> result) {
+        File sinkPath = createTestCsvSinkTable("MySink", "a int", "b bigint", "ts timestamp(3)");
 
-        DataFormatConverters.DataFormatConverter converterForDataType =
-                DataFormatConverters.getConverterForDataType(dataType);
-        List<Row> actualRows =
-                result.stream()
-                        .map(e -> (Row) converterForDataType.toExternal(e))
-                        .collect(Collectors.toList());
-        ;
+        String jsonPlan =
+                tableEnv.getJsonPlan("insert into MySink select a, b, ts from MyTable where b = 3");
+        tableEnv.executeJsonPlan(jsonPlan).await();
 
-        assertEquals(Arrays.asList(expected), actualRows);
+        assertResult(
+                Arrays.asList(
+                        "4,3," + formatSqlTimestamp(4000L),
+                        "5,3," + formatSqlTimestamp(5000L),
+                        "6,3," + formatSqlTimestamp(6000L)),
+                sinkPath);
     }
 
-    // ------------------------------------------------------------------------
-    //  test data
-    // ------------------------------------------------------------------------
+    @Test
+    public void testPushDowns() throws Exception {
+        createTestValuesSourceTable(
+                "MyTable",
+                JavaScalaConversionUtil.toJava(TestData.data3WithTimestamp()),
+                new String[] {
+                    "a int",
+                    "b bigint",
+                    "c varchar",
+                    "ts timestamp(3)",
+                    "watermark for ts as ts - interval '5' second"
+                },
+                "b",
+                new HashMap<String, String>() {
+                    {
+                        put("readable-metadata", "a:INT");
+                        put("filterable-fields", "a");
+                        put("enable-watermark-push-down", "true");
+                        put("partition-list", "b:1;b:2;b:3;b:4;b:5;b:6");
+                    }
+                });
 
-    private static final String[] CSV_LINES =
-            new String[] {
-                "Berlin,52.5167,13.3833,Germany,DE,Berlin,primary,3644826",
-                "San Francisco,37.7562,-122.4430,United States,US,California,,3592294",
-                "Beijing,39.9050,116.3914,China,CN,Beijing,primary,19433000"
-            };
+        File sinkPath = createTestCsvSinkTable("MySink", "a int", "ts timestamp(3)");
 
-    private static void writeFile(File testDir, String fileName, String[] lines)
-            throws IOException {
-        final File file = new File(testDir, fileName);
-        writeFileAtomically(file, lines);
+        String jsonPlan =
+                tableEnv.getJsonPlan(
+                        "insert into MySink select a, ts from MyTable where b = 3 and a > 4");
+        tableEnv.executeJsonPlan(jsonPlan).await();
+
+        assertResult(
+                Arrays.asList(
+                        "5," + formatSqlTimestamp(5000L),
+                        "6," + formatSqlTimestamp(6000L)),
+                sinkPath);
     }
 
-    private static void writeFileAtomically(final File file, final String[] lines)
-            throws IOException {
-        writeFileAtomically(file, lines, (v) -> v);
+    private static String formatSqlTimestamp(long timestamp){
+        return TimeFormats.SQL_TIMESTAMP_FORMAT.format(unixTimestampToLocalDateTime(timestamp));
     }
 
-    private static void writeFileAtomically(
-            final File file,
-            final String[] lines,
-            final FunctionWithException<OutputStream, OutputStream, IOException>
-                    streamEncoderFactory)
-            throws IOException {
-
-        // we don't use TMP_FOLDER.newFile() here because we don't want this to actually create a
-        // file,
-        // but just construct the file path
-        final File stagingFile =
-                new File(TMP_FOLDER.getRoot(), ".tmp-" + UUID.randomUUID().toString());
-
-        try (final FileOutputStream fileOut = new FileOutputStream(stagingFile);
-                final OutputStream out = streamEncoderFactory.apply(fileOut);
-                final OutputStreamWriter encoder =
-                        new OutputStreamWriter(out, StandardCharsets.UTF_8);
-                final PrintWriter writer = new PrintWriter(encoder)) {
-
-            for (String line : lines) {
-                writer.println(line);
-            }
-        }
-
-        final File parent = file.getParentFile();
-        assertTrue(parent.mkdirs() || parent.exists());
-
-        assertTrue(stagingFile.renameTo(file));
-    }
 }
